cdf6b582ed0121e08807f578e5675dcda17fae8f
David Espinosa <espinosadavid47@gmail.com>
2023-04-10 23:09:18 -0500

    (feat): adding script for data upload to local database

--- data_management.py
+++ data_management.py
@@ -1,1 +1,25 @@
-print('prueba')
+from sqlalchemy import create_engine, text
+import pandas as pd
+import pymysql
+from datetime import datetime
+
+data_frame = pd.read_csv("./MEN_SNIES_test_original.csv", engine="python", encoding="utf-8")
+print("CSV data successfully charged...")
+
+table_name = "men_data_info_csv"
+sql_engine = create_engine(
+    'mysql+pymysql://[user]:[password]@127.0.0.1/[database_name]', pool_recycle=3600)
+db_connection = sql_engine.connect()
+
+try:
+    frame = data_frame.to_sql(table_name, db_connection, if_exists='append')
+except ValueError as value_error:
+    print(value_error)
+except Exception as exception:
+    print(exception)
+else:
+    db_connection.execute(text("insert into men_data_info (ies, sector_ies, caracter_ies, departamento_domicilio_ies, municipio_domicilio_ies, programa_academico, nivel_academico, nivel_formacion, metodología, sexo, anio, semestre, graduados, matriculados) select ies, sector_ies, caracter_ies, departamento_domicilio_ies, municipio_domicilio_ies, programa_academico, nivel_academico, nivel_formacion, metodologia, sexo, anio, semestre, graduados, matriculados from men_data_info_csv;"))
+    db_connection.commit()
+    print("Data successfully charged to table men_data_info")
+finally:
+    db_connection.close()
