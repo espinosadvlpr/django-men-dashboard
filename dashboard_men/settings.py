35e9f218961fafe5ff0c8274a70b31ec74b2dac5
David Espinosa <38819699+espinosadvlpr@users.noreply.github.com>
2023-04-21 09:10:55 -0500

    (security): deleting key for security altert error

--- dashboard_men/settings.py
+++ dashboard_men/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!+5$kdmvyqky!=bn00!de^n47^p5xkscji%6ae8a8zuu#^69zd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
